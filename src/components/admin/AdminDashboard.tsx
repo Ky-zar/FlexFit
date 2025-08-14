973e6e87da260f946a749f7b8beee3217d34c757
Ahmed El-Kelawy <whitespade155@gmail.com>
2025-08-14 17:38:38 +0000

    Fix the list in the admin panel as it's overflowing.

--- src/components/admin/AdminDashboard.tsx
+++ src/components/admin/AdminDashboard.tsx
@@ -48,7 +48,7 @@ export default function AdminDashboard({ initialClasses, initialAnnouncements, i
 
             <main className="mt-8">
                 <Tabs defaultValue="classes">
-                    <TabsList className="grid w-full grid-cols-1 md:grid-cols-4 md:w-max">
+                    <TabsList className="grid w-full grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
                         <TabsTrigger value="classes">Manage Classes</TabsTrigger>
                         <TabsTrigger value="announcements">Manage Announcements</TabsTrigger>
                         <TabsTrigger value="memberships">
